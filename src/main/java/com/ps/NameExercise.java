8312cbd4fb8cbfa3e0de845532be931d0d40e8b5
Orchid <ovaldez@appdev.year.org>
2025-04-15 17:01:39 -0400

    first commit

--- src/main/java/com/ps/NameExercise.java
+++ src/main/java/com/ps/NameExercise.java
@@ -7,15 +7,20 @@ public class NameExercise {
 
 
         String fullName = "Orchid|Leonid|Valdez";
-        String[] names = fullName.split(" ");
+        String[] names = fullName.split("\\|");
 
         System.out.println ("Please enter your name: " + fullName);
         if (names.length > 0) {
             System.out.println("First Name:" + names[0]);
         }
 
-        System.out.println("Middle Name:" );
-        System.out.println("Last Name:" );
+        if (names.length > 1) {
+            System.out.println("Middle Name:" + names[1]);
+        }
+
+        if(names.length > 2) {
+        System.out.println("Last Name:" + names[names.length -1]);
+        }
 
 
 
