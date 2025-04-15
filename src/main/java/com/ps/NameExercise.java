832b8bb99379f0acda84cf99bca6f12dbe35276d
Orchid <ovaldez@appdev.year.org>
2025-04-15 13:56:58 -0400

    first commit

--- src/main/java/com/ps/NameExercise.java
+++ src/main/java/com/ps/NameExercise.java
@@ -1,4 +1,15 @@
 package com.ps;
 
 public class NameExercise {
+    public static void main(String[]args){
+
+        String firstName = "Orchid";
+        String middleName = "Leonid";
+        String lastName = "Valdez";
+        String suffix = "";
+        System.out.println ("Please enter your name:" + firstName +" "+ middleName +" "+ lastName + " "+ suffix);
+        System.out.printf(firstName);
+        System.out.printf(middleName);
+        System.out.printf(lastName);
+    }
 }
